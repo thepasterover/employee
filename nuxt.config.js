171899bd79d5e3b4c0d9c3920ee789f4cab6e55c
thepasterover <caped_crusad3r@protonmail.com>
2021-04-13 20:14:58 +0530

    Here we go again

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   mode: 'spa',
   router: {
-    base: '/employee-management-system/'
+    base: '/employee/'
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
